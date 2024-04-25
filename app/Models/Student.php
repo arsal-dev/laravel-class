a319b667405ea9781b411522a7570052f9ab0101
arsal-dev <arsalrajput173@gmail.com>
2024-04-25 19:00:12 +0500

    migrations

---
+++ app/Models/Student.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    // make:migration
+    // migrate
+    // migrate:rollback --step 3
+    // migrate:status
+    // migrate:reset
+    // migrate:fresh
+}
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
 Route::post('/contact', [ContactController::class, 'submit'])->name('contactSubmit');
+
+Route::resource('test', TestController::class);
